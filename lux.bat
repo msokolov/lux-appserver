6464e28aa6ef8e9cccb0879491f413b187d1c3e8
sokolov <sokolov@d2cdb213-60f8-4f1b-b4b2-f6b941818144>
2013-01-24 12:55:09 +0000

    packaging / config cleanup
    git-svn-id: http://falutin.net/svn/falutin/lux-appserver@1489 d2cdb213-60f8-4f1b-b4b2-f6b941818144

--- lux.bat
+++ lux.bat
@@ -1,1 +1,1 @@
-java -cp target/lux-appserver-0.0.1-SNAPSHOT.jar;target/dependency/\* lux.appserver.AppServer -p 8080 -c / -r src/test/webapp
+java -cp "lib\*" lux.appserver.AppServer -p 8080 -c / -r demo
