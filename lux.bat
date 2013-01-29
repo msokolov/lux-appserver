1ef82cc5d7643637d453819f6bd8fcd00ed93747
msokolov <sokolov@falutin.net>
2013-01-29 15:25:03 -0500

    fixing development config

--- lux.bat
+++ lux.bat
@@ -1,1 +1,1 @@
-java -cp "target\lux-appserver-0.5.jar;target\dependency\*;lib\*" lux.appserver.AppServer -p 8080 -c / -r demo
+java -cp ".;target\classes;target\dependency\*;lib\*" lux.appserver.AppServer
